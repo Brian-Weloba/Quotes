d42214d7ef5f309260d32bc73574faf290f57ef0
Brian <bweloba@gmail.com>
2021-03-28 13:00:50 +0300

    add quote class

---
+++ src/app/quote.ts
@@ -0,0 +1,12 @@
+export class Quote {
+  showDetails:boolean;
+  constructor(
+    public id:number,
+    public quote:string,
+    public author:string,
+    public user:string,
+    public date:Date
+  ){
+    this.showDetails = false;
+  }
+}
