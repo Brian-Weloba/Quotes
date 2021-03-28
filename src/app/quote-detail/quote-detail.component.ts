05f5e589b8e8a96e32b876b9487378ff1da7d790
Brian <bweloba@gmail.com>
2021-03-28 19:55:12 +0300

    add data binding

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -9,6 +10,11 @@ import { Quote } from '../quote';
 export class QuoteDetailComponent implements OnInit {
 
   @Input() quote: Quote;
+  @Output() deleteQuote = new EventEmitter<boolean>();
+
+  quoteDelete(toDelete:boolean){
+    this.deleteQuote.emit(toDelete);
+  }
 
   constructor() { }
 
